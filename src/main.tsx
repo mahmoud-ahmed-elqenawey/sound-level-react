4b4e3dc348a00194ef26ff506c073343b38e43d7
Mahmoud elqenawey <mahmoudelqenawey1@gmail.com>
2025-08-01 19:56:47 +0300

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { BrowserRouter, Route } from "react-router";
-import { Routes } from "react-router";
-import Login from "./pages/Login.tsx";
-import ProtectedRoute from "./pages/ProtectedRoute.tsx";
+// import { BrowserRouter, Route } from "react-router";
+// import { Routes } from "react-router";
+// import Login from "./pages/Login.tsx";
+// import ProtectedRoute from "./pages/ProtectedRoute.tsx";
 import App from "./App.tsx";
 import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    {/* <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route
@@ -22,7 +22,8 @@ createRoot(document.getElementById("root")!).render(
           }
         />
       </Routes>
-    </BrowserRouter>
+    </BrowserRouter> */}
+    <App />
     <Toaster position="top-center" />
   </StrictMode>
 );
